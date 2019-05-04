723278f55f8006975a1e8f914e730add6a77e21e
nickkiselev <min9897@mail.ru>
2019-05-04 11:30:58 +0300

    testcomm

--- src/Task1/Exercise2.java
+++ src/Task1/Exercise2.java
@@ -3,8 +3,6 @@ package Task1;
 import java.util.Scanner;
 
 public class Exercise2 {
-
-
 public static void main(String[] args) {
 
     double weightKG;
@@ -16,8 +14,6 @@ public static void main(String[] args) {
     weightKG=scanner.nextDouble();
 
     transfer(weightKG);
-
-
 }
 
 public static void transfer(double weightKG){
@@ -27,7 +23,5 @@ public static void transfer(double weightKG){
     System.out.println("Вес динозавра в тоннах равен "+ weightKG/Math.pow(10,3));
 
     System.out.println("Вес динозавра в граммах равен "+ weightKG*Math.pow(10,3));
-
 }
-
 }
